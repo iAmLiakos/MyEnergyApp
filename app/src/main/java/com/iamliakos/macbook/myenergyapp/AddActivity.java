3afb195d0568063d186e8f096ed30983f1a3abca
Ilias Papanikolaou <papaniki90@gmail.com>
2015-11-03 20:19:45 +0200

    Files
    Finally managed to create and save file locally.

--- app/src/main/java/com/iamliakos/macbook/myenergyapp/AddActivity.java
+++ app/src/main/java/com/iamliakos/macbook/myenergyapp/AddActivity.java
@@ -1,11 +1,28 @@
 package com.iamliakos.macbook.myenergyapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaScannerConnection;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -15,6 +32,9 @@ public class AddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button aboutButton = (Button) findViewById(R.id.savebutton);
+        aboutButton.setOnClickListener(saveButtonListener);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,4 +47,43 @@ public class AddActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+
+    public View.OnClickListener saveButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            savecall();
+        }
+    };
+
+    public static String STORETEXT="storetext.txt";
+    String string = "hello there";
+    public void savecall(){
+        try {
+
+
+
+            FileOutputStream out= openFileOutput(STORETEXT, Context.MODE_PRIVATE);
+            out.write(string.getBytes());
+            out.close();
+
+            Toast
+
+                    .makeText(this, "The contents are saved in the file.", Toast.LENGTH_LONG)
+
+                    .show();
+
+        }
+
+        catch (Throwable t) {
+
+            Toast
+
+                    .makeText(this, "Exception: "+t.toString(), Toast.LENGTH_LONG)
+
+                    .show();
+
+        }
+
+
+    }
 }
